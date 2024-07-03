4a217fd9e806276f710b0554796373bab468f90e
juheon.song <bootplus@naver.com>
2024-07-03 17:12:20 +0900

    [feature] length 0, other error

--- SimilarityChecker/similarity-checker.cpp
+++ SimilarityChecker/similarity-checker.cpp
@@ -1,11 +1,16 @@
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 class SimilarityChecker {
 public:
 	int length(const string& leftStr, const string& rightStr) {
-		if (leftStr.length() == rightStr.length()) return 60;
-		return 0;
+		int leftLen = leftStr.length();
+		int rightLen = rightStr.length();
+
+		if (leftLen == rightLen) return 60;
+		if (min(leftLen, rightLen) / (double)max(leftLen, rightLen) >= 0.5) return 0;
+		return (int)(1 - abs(leftLen - rightLen) / (double)min(leftLen, rightLen)) * 60;
 	}
 };
--- UnitTest/test.cpp
+++ UnitTest/test.cpp
@@ -8,3 +8,13 @@ TEST(SimilarityCheckerLength, Length60) {
 	SimilarityChecker checker;
 	EXPECT_EQ(60, checker.length("ASD", "DSA"));
 }
+
+TEST(SimilarityCheckerLength, Length0) {
+	SimilarityChecker checker;
+	EXPECT_EQ(0, checker.length("A", "BB"));
+}
+
+TEST(SimilarityCheckerLength, LengthOther) {
+	SimilarityChecker checker;
+	EXPECT_EQ(20, checker.length("AAABB", "BAA"));
+}
