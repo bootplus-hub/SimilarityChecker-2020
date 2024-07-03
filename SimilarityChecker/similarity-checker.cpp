adc61be76fb8c8f523e9020bc04686aa31fb5a80
juheon.song <bootplus@naver.com>
2024-07-03 22:29:11 +0900

    [refactoring] add checker interface & factory

--- SimilarityChecker/similarity-checker.cpp
+++ SimilarityChecker/similarity-checker.cpp
@@ -1,29 +1,107 @@
 #include <string>
 #include <algorithm>
+#include <memory>
+#include <unordered_map>
+#include <stdexcept>
+
+#define interface struct
 
 using namespace std;
 
+interface IChecker {
+	virtual int score(const string& leftStr, const string& rightStr) const = 0;
+	virtual bool isZeroScore(const string& leftStr, const string& rightStr) const = 0;
+	virtual bool isPerfectScore(const string& leftStr, const string& rightStr) const = 0;
+	virtual int getPerfectScoure() const = 0;
+};
+
+class LengthChecker : public IChecker {
+public:
+	static shared_ptr<IChecker> newInstance() {
+		return shared_ptr<IChecker>{ new LengthChecker() };
+	}
+
+	virtual int score(const string& leftStr, const string& rightStr) const {
+		const int LEFT_LEN = (int)leftStr.length();
+		const int RIGHT_LEN = (int)rightStr.length();
+		return (int)((1 - abs(LEFT_LEN - RIGHT_LEN) / (double)min(LEFT_LEN, RIGHT_LEN)) * PERFECT_SCOURE);
+	}
+
+	virtual bool isZeroScore(const string& leftStr, const string& rightStr) const {
+		const int LEFT_LEN = (int)leftStr.length();
+		const int RIGHT_LEN = (int)rightStr.length();
+		return min(LEFT_LEN, RIGHT_LEN) / (double)max(LEFT_LEN, RIGHT_LEN) < ZERO_SCOURE_LIMIT;
+	}
+
+	virtual bool isPerfectScore(const string& leftStr, const string& rightStr) const {
+		return leftStr.length() == rightStr.length();
+	}
+
+	virtual int getPerfectScoure() const {
+		return PERFECT_SCOURE;
+	}
+
+protected:
+	LengthChecker() {}
+
+private:
+	static constexpr int PERFECT_SCOURE = 60;
+	static constexpr double ZERO_SCOURE_LIMIT = 0.5;
+};
+
+
+class CheckerFactory {
+public:
+	enum CheckerType {
+		Length,
+	};
+
+	static CheckerFactory& getInstance() {
+		static CheckerFactory instance;
+		return instance;
+	}
+
+	shared_ptr<IChecker> getChecker(CheckerType checkerType) const {
+		auto entry = _store.find(checkerType);
+		if (entry == _store.end()) throw invalid_argument("Type not supported");
+		return entry->second;
+	}
+
+protected:
+	CheckerFactory() {
+		addChecker(Length, LengthChecker::newInstance());
+	}
+
+	CheckerFactory(const CheckerFactory&) = delete;
+	CheckerFactory& operator =(const CheckerFactory&) = delete;
+
+	virtual void addChecker(CheckerType checkerType, shared_ptr<IChecker> checkerPtr) {
+		_store[checkerType] = checkerPtr;
+	}
+private:
+	unordered_map<CheckerType, shared_ptr<IChecker>> _store;
+};
+
 class SimilarityChecker {
 public:
+	void injectChecker(const shared_ptr<IChecker>& checker) {
+		_checker = checker;
+	}
+
 	int score(const string& leftStr, const string& rightStr) {
 		int leftLen = (int)leftStr.length();
 		int rightLen = (int)rightStr.length();
 
-		if (isPerfectScore(leftLen, rightLen)) return 60;
-		if (isZeroScore(leftLen, rightLen)) return 0;
-		return calcScore(leftLen, rightLen);
+		if (checker().isPerfectScore(leftStr, rightStr)) return checker().getPerfectScoure();
+		if (checker().isZeroScore(leftStr, rightStr)) return 0;
+		return checker().score(leftStr, rightStr);
 	}
 
 protected:
-	bool isZeroScore(int leftLen, int rightLen) const {
-		return min(leftLen, rightLen) / (double)max(leftLen, rightLen) < 0.5;
+	IChecker& checker() const {
+		if (_checker.get() == nullptr) throw runtime_error("Please inject the checker first");
+		return *_checker;
 	}
-
-	bool isPerfectScore(int leftLen, int rightLen) const {
-		return leftLen == rightLen;
-	}
-
-	int calcScore(int leftLen, int rightLen) const {
-		return (int)((1 - abs(leftLen - rightLen) / (double)min(leftLen, rightLen)) * 60);
-	}
+private:
+	shared_ptr<IChecker> _checker;
 };
--- UnitTest/test.cpp
+++ UnitTest/test.cpp
@@ -9,15 +9,25 @@ public:
 	SimilarityChecker checker;
 };
 
+TEST_F(SimilarityCheckerFixture, RuntimeError) {
+	EXPECT_THROW(checker.score("ASD", "DSA"), runtime_error);
+}
+
 TEST_F(SimilarityCheckerFixture, Length60) {
+	checker.injectChecker(CheckerFactory::getInstance().getChecker(CheckerFactory::Length));
+
 	EXPECT_EQ(60, checker.score("ASD", "DSA"));
 }
 
 TEST_F(SimilarityCheckerFixture, Length0) {
+	checker.injectChecker(CheckerFactory::getInstance().getChecker(CheckerFactory::Length));
+
 	EXPECT_EQ(0, checker.score("A", "BB"));
 }
 
 TEST_F(SimilarityCheckerFixture, LengthOther) {
+	checker.injectChecker(CheckerFactory::getInstance().getChecker(CheckerFactory::Length));
+
 	EXPECT_EQ(20, checker.score("AAABB", "BAA"));
 	EXPECT_EQ(30, checker.score("AA", "AAE"));
 }
